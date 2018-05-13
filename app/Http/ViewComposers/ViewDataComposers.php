55fc3f34c7d396baaef0e4212b28bcb842825d18
kientranutc <kienkienutc95@gmail.com>
2018-05-13 18:25:02 +0700

    add composer service

---
+++ app/Http/ViewComposers/ViewDataComposers.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use App\Repositories\Banner\BannerRepositoryInterface;
+use App\Repositories\Categories\CategoryRepositoryInterface;
+use App\Repositories\Comment\CommentRepositoryInterface;
+use App\Repositories\Order\OrderRepositoryInterface;
+use Illuminate\View\View;
+
+class ViewDataComposers
+{
+    /**
+     * The repository implementation.
+     *
+     */
+    protected $category;
+    protected  $banner;
+    protected $comment;
+    protected $order;
+    /**
+     * Create a new view  composer.
+     *
+     * @return void
+     */
+
+    public function __construct()
+    {
+//        $this->category = $category;
+//        $this->banner = $banner;
+//        $this->comment = $comment;
+//        $this->order = $order;
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+//        $dataCategory = $this->category->getCategoryActive();
+//        $dataBanner = $this->banner->getBannerActive();
+//        $countCommentDayNow = $this->comment->countCommentDayNow ();
+//        $countOrderDayNow = $this->order->countOrderDayNow ();
+//        $moneyOrder = $this->order->getMoneyOrder();
+//        $view->with(
+//            [
+//                'category'=> $dataCategory,
+//                'banner' => $dataBanner,
+//                'countComment' => $countCommentDayNow,
+//                'countOrder' => $countOrderDayNow,
+//                'moneyOrder' => $moneyOrder
+//
+//            ]);
+
+    }
+}
