ecfad21d47ee12e4f66c13a741670ad5854a25e0
kientranutc <kienkienutc95@gmail.com>
2018-05-15 06:51:15 +0700

    add service lg

---
+++ config/service.php
@@ -0,0 +1,39 @@
+<?php
+    return [
+      'service_en' => [
+          [
+              'image' => '',
+              'title' => 'Information and Analysis',
+              'content' => 'Cao hospitality provide full range of Marketing Research and Analysis, that is essential for Decision Making. We help to develop and implement most efficient Business Strategies to promote and grow your business.'
+          ],
+          [
+              'image' => '',
+              'title' => 'ITailor-made Managing and consulting',
+              'content' => 'Our experts are ready to support you on any stage of your business thus having us not only as advisors, but as reliable partners with more than 20 years of experience invested in your business.'
+          ],
+          [
+          'image' => '',
+          'title' => 'Pr - Marketing',
+          'content' => 'With our most experienced experts, know-hows and established connections, Cao hospitality will give the effective Marketing and Sales consulting at suitable cost to right market at right time. '
+            ]
+
+      ],
+      'service_vi' => [
+          [
+              'image' => '',
+              'title' => 'Lập kế hoạch – Phát triển kinh doanh',
+              'content' => 'Cao hospitality là đơn vị chuyên sâu trong lĩnh vực nghiên cứu sản phẩm, phân tích chuyên sâu, lên kế hoạch, chiến lược, phát triển khai thác kinh doanh. Đội ngũ chuyên viên của chúng tôi tự hào khi đã đem lại lợi ích kinh doanh cho rất nhiều các thương hiệu lớn tại Việt Nam '
+          ],
+          [
+              'image' => '',
+              'title' => 'Tư vấn quản lý',
+              'content' => 'Với kinh nghiệm tư vấn quản lý phong phú, chúng tôi đưa ra các phương án quản lý và các quy trình tiêu chuẩn phù hợp với tùng đơn vị kinh doanh nhằm  đảm bảo việc hoạt động chuyên nghiệp, hiệu quả cao.'
+          ],
+          [
+              'image' => '',
+              'title' => 'Pr - Marketing',
+              'content' => 'Chúng tôi hiểu rằng kế hoạch Marketing đóng một phần rất quan trọng việc phát triển thương hiệu. Cao hospitality đem lại những chương trình Marketing hiệu quả với chi phí hợp lý, giúp khách hàng truyền đạt đúng thông điệp đến đúng đối tượng mục tiêu vào đúng thời điểm.'
+          ]
+      ]
+    ];
+?>
