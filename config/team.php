b245e4ea6d9914713d35eedef2f8c03e029cc758
kientranutc <kienkienutc95@gmail.com>
2018-05-14 20:13:13 +0700

    add all

---
+++ config/team.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+  'team_en' => [
+
+  ],
+  'team_vi' => [
+      [
+          'image' => '',
+          'name' => 'Nguyễn Thị Thu Huyền',
+          'level' => 'HĐQT –  NGƯỜI SÁNG LẬP',
+          'exp' => [
+              1 => 'Tốt nghiệp Học Viện Tài Chính chuyên ngành tài chính ngân hàng. Ngoài ra Bà còn tham gia các khóa học đào tạo nghiệp vụ tài chính doanh nghiệp của PhòngThương mại và Công nghiệp Versailles C.C.I.V.',
+              2 => 'Trên 10 năm kinh nghiệm tại các vị trí kế toán trưởng tại Tập đoàn Đại Dương; Tập đoàn Sungroup',
+              3 => 'Chủ tịch Hội đồng quản trị Công ty Cổ phần Truyền thông Kim Cương - Lotte Hàn Quốc.',
+              4 => 'Trưởng ban kiểm toán nội bộ của Công ty CP TVShopping, Thời báo Doanh nhân, Công ty cổ phần Greenmedia',
+              5 =>  'Giám đốc tài Chính tại STS Hospitality.'
+          ]
+      ],
+      [
+          'image' => '',
+          'name' => 'Cao Đức Trung',
+          'level' => 'HĐQT – ĐỒNG SÁNG LẬP GIÁM ĐỐC TÀI CHÍNH ',
+          'exp' => [
+              1 => 'Tốt nghiệp Học Viện Tài Chính chuyên ngành tài chính doanh nghiệp',
+              2 => 'Tốt nghiệp loại giỏi khóa học Quản lý khách sạn theo tiêu chuẩn nghề du lịch Việt Nam do Trung tâm Đào tạo Quốc tế Pegasus thuộc Tập đoàn giáo dục KinderWorld đào tạo và cấp chứng chỉ',
+              3 => 'Phó phòng Kiểm toán nội bộ tại Tổng Công ty Hàng hải Việt Nam.',
+              4 => 'Trưởng phòng Kiểm soát tuân thủ tại Tập đoàn SUNGROUP (đơn vị sở hữu Thương hiệu SUNWORLD và các bất động sản nghỉ dưỡng nổi tiếng như Intercontinent Đà Nẵng, Primer Village, khách sạn Novotel Đà Nẵng,…).',
+              5 => 'Kế toán trưởng tại Tập đoàn Thành Nam'
+          ]
+      ],
+      [
+          'image' => '',
+          'name' => 'Phạm Bá Tước',
+          'level' => 'HĐQT – ĐỒNG SÁNG LẬP GIÁM ĐỐC ĐIỀU HÀNH',
+          'exp' => [
+              1 => 'Cử nhân chuyên ngành kinh tế quản trị kinh doanh tổng hợp.',
+              2 => 'Đào tạo nghiệp vụ chuyên ngành khách sạn tại Hàn quốc',
+              3 => 'Với hơn 20 năm kinh nghiệm trong ngành  khách sạn',
+              4 => 'Khách sạn Hanoi Deawoo – Phụ trách kinh doanh hội thảo & tiệc',
+              5 => 'Tập đoàn SWISS BEL – Phụ trách kinh doanh Mihrin Hạ long',
+              6 => 'Khách sạn Lan Viên – Tổng Quản Lý ',
+              7 => 'Tập đoàn Nam Cường – Phụ trách khối kinh doanh khách sạn',
+              8 => 'Khách san THE REED Ninh Bình – Giám đốc kinh doanh & QC',
+              9 => 'Giám đốc phát triển kinh doanh tại STS Hospitality',
+              10 => 'Giám đốc điều hành khách sạn Hòa Bình 4 sao'
+          ]
+      ],
+      [
+          'image' => '',
+          'name' => 'Phạm Bá Tước',
+          'level' => 'HĐQT – ĐỒNG SÁNG LẬP GIÁM ĐỐC ĐIỀU HÀNH',
+          'exp' => [
+              1 => 'Cử nhân chuyên ngành kinh tế quản trị kinh doanh tổng hợp.',
+              2 => 'Đào tạo nghiệp vụ chuyên ngành khách sạn tại Hàn quốc',
+              3 => 'Với hơn 20 năm kinh nghiệm trong ngành  khách sạn',
+              4 => 'Khách sạn Hanoi Deawoo – Phụ trách kinh doanh hội thảo & tiệc',
+              5 => 'Tập đoàn SWISS BEL – Phụ trách kinh doanh Mihrin Hạ long',
+              6 => 'Khách sạn Lan Viên – Tổng Quản Lý ',
+              7 => 'Tập đoàn Nam Cường – Phụ trách khối kinh doanh khách sạn',
+              8 => 'Khách san THE REED Ninh Bình – Giám đốc kinh doanh & QC',
+              9 => 'Giám đốc phát triển kinh doanh tại STS Hospitality',
+              10 => 'Giám đốc điều hành khách sạn Hòa Bình 4 sao'
+          ]
+      ]
+  ]
+
+];
+?>
