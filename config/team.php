14af6514683b6d06721bf70f36a79ed210dcf174
kientranutc <kienkienutc95@gmail.com>
2018-05-15 06:43:22 +0700

    add team

--- config/team.php
+++ config/team.php
@@ -2,11 +2,51 @@
 
 return [
   'team_en' => [
+      [
+          'image' => 'ns1.png',
+          'name' => 'Nguyen Thi Thu Huyen',
+          'level' => 'Founder - Chair woman',
+          'exp' => [
+              1 => 'Degree of Bachelor – Academy of finance ',
+              2 => 'Chef accountant certification -  National economics university',
+              3 => 'Over 10 years Experience as chief accountant at Ocean group – Ocean group ',
+              4 => ' Chair woman – Diamond media jsc., – Lotte.',
+              5 =>  'Giám đốc tài Chính tại STS Hospitality.'
+          ]
+      ],
+      [
+          'image' => 'ns2.png',
+          'name' => 'Cao Duc Trung',
+          'level' => 'Co-founder Finance manager',
+          'exp' => [
+              1 => 'Degree of Bachelor - Academy of Finance',
+              2 => 'Excellent Graduated in Hospitality Management - Pegasus International Training Center of the Kinder World Education Group ',
+              3 => 'Deputy Director of Internal Audit Department at Vietnam National Shipping Lines',
+              4 => ' Head of Compliance Control Department at SUNGROUP Corporation (the owner of SUNWORLD brand name and famous resort real estate such as Intercontinental Da Nang, Primer Village, Novotel Da nang, ...).',
+              5 =>  'Chief Accountant at Thanh Nam Group'
+          ]
+      ],
+      [
+          'image' => 'ns3.jpg',
+          'name' => 'Pham Ba Tuoc',
+          'level' => 'Co-founder General manager',
+          'exp' => [
+              1 => 'Bachelor of Economics in Business Administration.. ',
+              2 => 'Professional hotel skills trained in Korea',
+              3 => 'Hanoi Deawoo 5* – Banquet Sales Manager',
+              4 => 'SWISS BEL group – DOS Mihrin Ha long  4*',
+              5 =>  'Lan Viên  hotel  4*–  GM',
+              6 => 'Nam Cuong Group– DOS',
+              7 => 'THE REED hotel Ninh Binh 4*–  DOSM',
+              8 => 'STS Hospitality  - DOS',
+              9 => 'HOA BINH HOTEL 4* - GM - DOSM'
+          ]
+      ],
 
   ],
   'team_vi' => [
       [
-          'image' => '',
+          'image' => 'ns1.png',
           'name' => 'Nguyễn Thị Thu Huyền',
           'level' => 'HĐQT –  NGƯỜI SÁNG LẬP',
           'exp' => [
@@ -18,7 +58,7 @@ return [
           ]
       ],
       [
-          'image' => '',
+          'image' => 'ns2.png',
           'name' => 'Cao Đức Trung',
           'level' => 'HĐQT – ĐỒNG SÁNG LẬP GIÁM ĐỐC TÀI CHÍNH ',
           'exp' => [
@@ -30,7 +70,7 @@ return [
           ]
       ],
       [
-          'image' => '',
+          'image' => 'ns3.jpg',
           'name' => 'Phạm Bá Tước',
           'level' => 'HĐQT – ĐỒNG SÁNG LẬP GIÁM ĐỐC ĐIỀU HÀNH',
           'exp' => [
@@ -47,20 +87,19 @@ return [
           ]
       ],
       [
-          'image' => '',
-          'name' => 'Phạm Bá Tước',
-          'level' => 'HĐQT – ĐỒNG SÁNG LẬP GIÁM ĐỐC ĐIỀU HÀNH',
+          'image' => 'ns3,jpg',
+          'name' => 'Bùi Thanh Nga',
+          'level' => 'TV HĐQT GIÁM ĐỐC KINH DOANH – MARKETING ',
           'exp' => [
-              1 => 'Cử nhân chuyên ngành kinh tế quản trị kinh doanh tổng hợp.',
-              2 => 'Đào tạo nghiệp vụ chuyên ngành khách sạn tại Hàn quốc',
-              3 => 'Với hơn 20 năm kinh nghiệm trong ngành  khách sạn',
-              4 => 'Khách sạn Hanoi Deawoo – Phụ trách kinh doanh hội thảo & tiệc',
-              5 => 'Tập đoàn SWISS BEL – Phụ trách kinh doanh Mihrin Hạ long',
-              6 => 'Khách sạn Lan Viên – Tổng Quản Lý ',
-              7 => 'Tập đoàn Nam Cường – Phụ trách khối kinh doanh khách sạn',
-              8 => 'Khách san THE REED Ninh Bình – Giám đốc kinh doanh & QC',
-              9 => 'Giám đốc phát triển kinh doanh tại STS Hospitality',
-              10 => 'Giám đốc điều hành khách sạn Hòa Bình 4 sao'
+              1 => 'Quản trị Kinh doanh & Ngành Khách sạn',
+              2 => 'Với hơn 20 năm kinh nghiệm trong ngành  khách sạn ',
+              3 => 'Khách sạn Hanoi Deawoo – Phụ trách hội thảo & tiệc',
+              4 => 'Khách sạn Horison – Quản lý kinh doanh',
+              5 => 'Khách san Sunway– Quản lý kinh doanh',
+              6 => 'Khách san Sunway– Quản lý kinh doanh cấp cao',
+              7 => 'Công Ty quản lý khách sạn H&K Hospitality – Quản lý kinh doanh chuỗi ',
+              8 => 'Khu  Điều  Dưỡng  Cán Bộ Hà Nội tại Đồ Sơn – Giám đốc kinh doanh',
+              9 => 'Khách sạn Westlake Vĩnh Phúc – Giám đốc kinh doanh',
           ]
       ]
   ]
